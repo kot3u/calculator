0e5c5f1afdb2452d7a7b047e2120dfe43031ca1a
kot3u <125024243+kot3u@users.noreply.github.com>
2023-02-27 19:14:35 +0100

    working version, lacking clear

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
 let result = 0;
-let add = (a, b) => {return a + b;};
-let substract = (a, b) => {return a - b;};
-let divide = (a, b) => { return a / b;};
-let multiply = (a, b) => {return a * b;};
+let mode = 'a';
+let a = '';
+let b = '';
+let operator = '';
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const output = document.querySelector('.result');
+const equals = document.querySelector('#equals');
 
+let add = (a, b) => { return a + b; };
+let substract = (a, b) => { return a - b; };
+let divide = (a, b) => { return a / b; };
+let multiply = (a, b) => { return a * b; };
 
-let operate = (a, b, operator) => {
+let operate = () => {
+  if(a === '' || operator === '' || b === ''){
+  
+  };
+
+  a = parseFloat(a);
+  b = parseFloat(b);
   if(operator === '+'){
     result = add(a, b);
   }
   else if (operator === '-') {
     result = substract(a, b);
   }
-  else if (operator === '') {
+  else if (operator === '/') {
     result = divide(a, b);
   }
-  else if (operator === '+') {
+  else if (operator === '*') {
     result = multiply(a, b);
+  }
+  else {
+    output.textContent = 'something went wrong, start over.';
   };
-  return result;
+  output.textContent = result;
+  a = result;
+  b = '';
+  mode = 'a';
+  operator= '';
 };
 
-const numbers = document.querySelectorAll('.num');
-const operators = document.querySelectorAll('.operator')
+let appendNumber = (e) => {
+  if(mode === 'a'){
+    a += e.target.textContent;
+    output.textContent = a;
+  }
+  else if(mode === 'b'){
+    b += e.target.textContent;
+    output.textContent = b;
+  };
+}
 
 numbers.forEach(number => {
-  number.addEventListener('click', (e) => {
-    console.log(e.target.innerHTMl)
-  })
+  number.addEventListener('click', appendNumber);
 });
+
+let setOperator = (e) => {
+  if(operator != '' ){
+    operate()
+  };
+  operator = e.target.textContent;
+  mode = 'b'
+};
+
+operators.forEach(operator => {
+  operator.addEventListener('click', setOperator);
+})
+
+equals.addEventListener('click', operate);
